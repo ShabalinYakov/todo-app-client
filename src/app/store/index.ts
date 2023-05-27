e94ea506a1321a1887e1be79b89a096d6d12faa2
Shabalin_Yakov <shab.yakov@ya.ru>
2023-05-28 00:09:34 +0500

    feat: starter frontend template created

---
+++ src/app/store/index.ts
@@ -0,0 +1,3 @@
+export { useStore } from './use-store';
+export { RootStore } from './root-store';
+export { RootStoreContext } from './root-store-context';
---
+++ src/app/store/root-store.ts
@@ -0,0 +1,3 @@
+export class RootStore {
+  test = 'test';
+}
