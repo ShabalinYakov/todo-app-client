d923f97c3b2063d78092a4157ccdf0ead1ff1112
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-01 02:15:52 +0500

    feat: added path /login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import { useRoutes } from 'react-router-dom';
 
+import LoginPage from './login-page';
+
 const _Routing = (): React.ReactElement<any, string | React.JSXElementConstructor<any>> | null => {
   const elements = useRoutes([
-    {
-      path: '/',
-      element: <h1>Initial</h1>,
-    },
+    { path: '/', element: <h1>Initial</h1> },
+    { path: '/login', element: <LoginPage /> },
   ]);
   return elements;
 };
