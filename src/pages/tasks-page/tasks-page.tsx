5b954cab6cf6de856007ca3a8a7801ccb84dbfa9
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-03 12:13:41 +0500

    feat: added prototype Tasks page

---
+++ src/pages/tasks-page/tasks-page.tsx
@@ -0,0 +1,11 @@
+import { observer } from 'mobx-react-lite';
+
+const _TasksPage = () => {
+  return (
+    <>
+      <h1>Страница задач</h1>
+    </>
+  );
+};
+
+export const TasksPage = observer(_TasksPage);
