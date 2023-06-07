199a37be1405ec8bb078d8fe16d73ff72d5374a6
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-07 22:58:46 +0500

    changeset: common update, add style variable-beta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { useRoutes } from 'react-router-dom';
 
-import LoginPage from './login-page';
+import { LoginPage } from './login-page';
 import { TasksPage } from './tasks-page/tasks-page';
 
 const _Routing = () => {
