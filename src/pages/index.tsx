77a4657414aaa04ce15cdf2181142cd28003835c
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-08 14:55:21 +0500

    feat: added new path "/logout" for exit app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,13 @@ import { useRoutes } from 'react-router-dom';
 import { LoginPage } from './login-page';
 import { TasksPage } from './tasks-page/tasks-page';
 
+import { Logout } from 'features/logout';
+
 const _Routing = () => {
   const elements = useRoutes([
     { path: '/', element: <TasksPage /> },
     { path: '/login', element: <LoginPage /> },
+    { path: '/logout', element: <Logout /> },
   ]);
   return elements;
 };
