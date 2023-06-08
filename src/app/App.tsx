dbc8de63e46cb48c78270548bcef1b4d6fbde691
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-08 14:52:48 +0500

    feat: added useEffect for checkAuth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from 'react';
+
 import { withProviders } from './providers';
-
-import Routing from 'pages';
-
 import './App.scss';
 
+import { useStore } from 'app';
+import Routing from 'pages';
+
 const _App = () => {
+  const { sessionStore } = useStore();
+
+  useEffect(() => {
+    sessionStore.checkAuth();
+  });
+
   return <Routing />;
 };
 
