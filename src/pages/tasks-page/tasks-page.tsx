b5a4d2e35a8c43204297606af1130fe2010c8d3a
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-11 14:43:28 +0500

    changeset: added useEffect for loadTasks, added TasksList, added minimal styles

--- src/pages/tasks-page/tasks-page.tsx
+++ src/pages/tasks-page/tasks-page.tsx
@@ -1,14 +1,29 @@
 import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { PrivateRoute } from 'features/private-route';
+import { useStore } from 'app';
+
+import { TasksList } from 'features/tasks-list';
+import './tasks-page.scss';
 
 const _TasksPage = () => {
+  const { tasksStore } = useStore();
+
+  useEffect(() => {
+    if (tasksStore.tasks.length === 0) {
+      tasksStore.loadTasks();
+    }
+  }, [tasksStore]);
+
   return (
-    <PrivateRoute>
+    <>
       <h1>Страница задач</h1>
       <Link to="/logout">Logout</Link>
-    </PrivateRoute>
+      <div className="tasks-page__tasks-wrapper">
+        <TasksList tasks={tasksStore.tasks} isLoad={tasksStore.isLoading} />
+      </div>
+    </>
   );
 };
 
