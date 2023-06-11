80ca894fd60d7e68cace0fee525fd8d881dc97f6
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-11 14:09:30 +0500

    feat: added type Task for entiity tasks

---
+++ src/entities/tasks/model/types.ts
@@ -0,0 +1,12 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+  priority: string;
+  deadline: string;
+  creator: string;
+  responsible: string;
+  created_at: string;
+  updated_at: string;
+}
