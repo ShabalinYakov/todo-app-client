b8bac8e4057e3a2b15181d0121827b81c0329bed
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-11 14:15:55 +0500

    feat: added tasksStore in rootStore

--- src/app/store/root-store.ts
+++ src/app/store/root-store.ts
@@ -1,5 +1,7 @@
 import { SessionStore } from 'entities/session';
+import { TasksStore } from 'entities/tasks';
 
 export class RootStore {
   sessionStore = new SessionStore();
+  tasksStore = new TasksStore();
 }
