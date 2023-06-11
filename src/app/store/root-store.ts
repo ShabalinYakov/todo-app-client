d27721da440d9ae8f326d19451349f680d3258bb
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-11 15:26:49 +0500

    feat: added priorities store in root-store

--- src/app/store/root-store.ts
+++ src/app/store/root-store.ts
@@ -1,7 +1,9 @@
+import { PrioritiesStore } from 'entities/priorities';
 import { SessionStore } from 'entities/session';
 import { TasksStore } from 'entities/tasks';
 
 export class RootStore {
   sessionStore = new SessionStore();
   tasksStore = new TasksStore();
+  prioritiesStore = new PrioritiesStore();
 }
