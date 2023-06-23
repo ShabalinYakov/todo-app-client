c67d7605f9a820e4a7bca337c6662450b217463d
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-23 22:34:55 +0500

    refactor: entities tasks

--- src/entities/tasks/model/store.ts
+++ src/entities/tasks/model/store.ts
@@ -3,11 +3,10 @@ import { makeAutoObservable, runInAction } from 'mobx';
 import { Task } from './types';
 
 import { tasksApi } from '../api';
-import { getEndOfDayInMs, getEndOfWeekInMs } from '../lib/date-helpers';
 
 export class TasksStore {
   isLoading = false;
-  tasks: Task[] = [];
+  tasksViewer: Task[] = [];
 
   constructor() {
     makeAutoObservable(this);
@@ -17,39 +16,12 @@ export class TasksStore {
     this.isLoading = bool;
   }
 
-  setTasks(data: Task[]) {
-    this.tasks = data;
-  }
-
-  get tasksForToday() {
-    const dayEnd = getEndOfDayInMs();
-
-    return this.tasks
-      .filter((task) => Date.parse(task.deadline) < dayEnd)
-      .sort((prevTask, nextTask) => Date.parse(prevTask.deadline) - Date.parse(nextTask.deadline));
-  }
-
-  get tasksForWeek() {
-    const dayEnd = getEndOfDayInMs();
-    const weekEnd = getEndOfWeekInMs();
-    return this.tasks
-      .filter((task) => Date.parse(task.deadline) > dayEnd && Date.parse(task.deadline) < weekEnd)
-      .sort((prevTask, nextTask) => Date.parse(prevTask.deadline) - Date.parse(nextTask.deadline));
-  }
-
-  get tasksForFuture() {
-    const weekEnd = getEndOfWeekInMs();
-    return this.tasks
-      .filter((task) => Date.parse(task.deadline) > weekEnd)
-      .sort((prevTask, nextTask) => Date.parse(prevTask.deadline) - Date.parse(nextTask.deadline));
-  }
-
-  loadTasks = async () => {
+  loadViewerTasks = async () => {
     this.setLoading(true);
     try {
       const tasks = await tasksApi.getTasks();
       runInAction(() => {
-        this.setTasks(tasks);
+        this.tasksViewer = tasks;
       });
     } catch (error) {
       console.log(error);
@@ -59,6 +31,6 @@ export class TasksStore {
   };
 
   getTaskById(id: string) {
-    return this.tasks.find((task) => task.id === id);
+    return this.tasksViewer.find((task) => task.id === id);
   }
 }
